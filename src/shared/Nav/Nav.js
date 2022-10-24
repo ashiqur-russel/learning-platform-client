2cb27ba87bc84e4a9cbc0a4083058102ad4c9183
ashiqurras <ashiqurrussel2@gmail.com>
2022-10-24 22:19:35 +0200

    Router set up and Nav created

---
+++ src/shared/Nav/Nav.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.jpg";
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth={1.5}
+  stroke="currentColor"
+  className="w-6 h-6"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+  />
+</svg>;
+
+const Nav = () => {
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "COURSES", link: "/courses" },
+    { name: "BLOG'S", link: "/blog" },
+    { name: "LOGIN", link: "/login" },
+    { name: "REGISTER", link: "/register" },
+  ];
+  let [open, setOpen] = useState(false);
+  return (
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
+        >
+          <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
+        </div>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          <span name={open ? "close" : "menu"}>
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </span>
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div class="overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600">
+          <svg
+            class="absolute -left-1 w-12 h-12 text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
