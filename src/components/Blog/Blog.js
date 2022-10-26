a4a3bc87264f594a731f21e6497dfe2bb13f6355
ashiqurras <ashiqurrussel2@gmail.com>
2022-10-26 21:45:59 +0200

    Download pdf button

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import "./Blog.css";
+
 const Blog = () => {
   return (
     <div>
--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -2,50 +2,57 @@ import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { Image } from "react-bootstrap";
 import "./CourseDetails.css";
+
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
+
 const CourseDetails = () => {
   const course = useLoaderData();
 
   const { details, title, image_url, _id } = course;
   return (
     <>
-      <section className="light">
-        <div className="container py-4">
-          <h1 className="h1 text-center" id="pageHeaderTitle">
-            Enroll today
-          </h1>
+      <div className="container py-4">
+        <h1 className="h1 text-center" id="pageHeaderTitle">
+          Enroll today
+        </h1>
+        <p className="text-center">
+          <Pdf targetRef={ref} filename="code-example.pdf">
+            {({ toPdf }) => (
+              <button className="btn btn-warning" onClick={toPdf}>
+                Download Pdf
+              </button>
+            )}
+          </Pdf>
+        </p>
 
-          <article className="postcard dark yellow">
-            <Image
-              className="postcard__img"
-              src={image_url}
-              alt="Image Title"
-            />
-            <div className="postcard__text">
-              <h1 className="postcard__title yellow">
-                <p href="#">{title}</p>
-              </h1>
-              <div className="postcard__subtitle small">
-                <time datetime="2020-05-25 12:00:00">
-                  <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-                </time>
-              </div>
-              <div className="postcard__bar"></div>
-              <div className="postcard__preview-txt">{details}</div>
-              <ul className="postcard__tagbox">
-                <Link to={`/cart/${_id}`}>
-                  {" "}
-                  <button
-                    className="btn btn-warning w-100"
-                    variant="outline-secondary"
-                  >
-                    Get Premium Access{" "}
-                  </button>
-                </Link>{" "}
-              </ul>
+        <article className="postcard dark yellow" ref={ref}>
+          <Image className="postcard__img" src={image_url} alt="Image Title" />
+          <div className="postcard__text">
+            <h1 className="postcard__title yellow">
+              <p href="#">{title}</p>
+            </h1>
+            <div className="postcard__subtitle small">
+              <time dateTime="2020-05-25 12:00:00">
+                <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+              </time>
             </div>
-          </article>
-        </div>
-      </section>
+            <div className="postcard__bar"></div>
+            <div className="postcard__preview-txt">{details}</div>
+            <ul className="postcard__tagbox">
+              <Link to={`/cart/${_id}`}>
+                {" "}
+                <button
+                  className="btn btn-warning w-100"
+                  variant="outline-secondary"
+                >
+                  Get Premium Access{" "}
+                </button>
+              </Link>{" "}
+            </ul>
+          </div>
+        </article>
+      </div>
     </>
   );
 };
